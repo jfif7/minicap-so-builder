57da29b62a0e9091445ec4b6711079de76885900
jfif7 <github@onino.tw>
2024-12-07 12:19:27 +0800

    Update image name

--- checkout.sh
+++ checkout.sh
@@ -2,7 +2,7 @@ export AOSP_DIR="$(pwd)/repo"
 BRANCH="android-security-12.1.0_r4"
 
 docker run -ti --rm \
-    -v "${AOSP_DIR}/${BRANCH}/:/aosp" \
-    -v "${AOSP_DIR}/.gitcookies:/root/.gitcookies:ro" \
-    jdk21 \
+    -v ${AOSP_DIR}/${BRANCH}/:/aosp \
+    -v .gitcookies:/root/.gitcookies:ro \
+    sahadusa/aosp:jdk21 \
     /aosp.sh checkout-branch --no-mirror $BRANCH -c -j8
